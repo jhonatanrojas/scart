4351e5d4d3137a01ef53951069b7ea3810932895
johandry <rojas.rojas440@gmail.com>
2022-09-02 14:48:03 -0400

    traduccion_es

---
+++ lang/es/install.php
@@ -0,0 +1,138 @@
+<?php
+return [
+    'info' => [
+        'about' => '
+        S-Cart es un proyecto de sitio web de comercio electrónico gratuito para empresas, construido en el marco de Laravel (PHP y Mysql)',
+        'about_pro' => '
+        ¡La mejor plataforma de comercio electrónico de Laravel para empresas!',
+        'about_us' => '-Sobre nosotras, por favor visite <a target="_new" href="https://s-cart.org/">S-Cart\'s página principal</a>',
+        'document' => '- 
+        Guía de instalación <a target="_new" href="https://s-cart.org/docs/master/installation.html">AQUÍ</a>',
+        'version' => 'Versions',
+        'terms' => '<span style="color:red">*</span> Lea los términos antes de instalar <a target="_new" href="https://s-cart.org/license.html">AQUÍ</a>.',
+        'terms_pro' => '<span style="color:red">*</span> Lea los términos antes de instalar <a target="_new" href="https://s-cart.org/pro.html">AQUÍ</a>.',
+        '',
+    ],
+    'env' => [
+        'process' => '
+        Generando archivo .env',
+        'error_open' => 'No se puede abrir el archivo .env.example',
+        'process_sucess' => 'Generate file .env success!',
+        'error' => 'Error al generar archivo .env',
+        'nofound' => '
+        ¡Archivo .env.example no encontrado!',
+    ],
+    'key' => [
+        'process' => 'Generando clave API',
+        'process_sucess' => '
+        ¡Genera el éxito de la clave API!',
+        'error' => '
+        Error al generar la clave API',
+    ],
+    'database' => [
+        'process_sucess' => '
+        ¡Inicialización exitosa!',
+        'process_sucess_1' => '
+        ¡Crea una nueva mesa con éxito como administrador!',
+        'process_sucess_2' => '
+        ¡Crea un nuevo éxito en la tienda de mesas!',
+        'process_sucess_3' => '
+        Insertar demostración de datos ¡éxito predeterminado!',
+        'process_sucess_4' => '
+        ¡Inserción local de datos exitosa!',
+        'process_sucess_5' => '
+        ¡Inserte la muestra de datos con éxito!',
+        'file_notfound' => 'No se puede encontrar el archivo .sql',
+        'error' => '
+        Error al inicializar la base de datos',
+        'error_1' => '
+        Error al crear administrador de tabla',
+        'error_2' => '
+        Error al crear tienda de mesa',
+        'error_3' => '
+        Error al insertar la demostración predeterminada de datos',
+        'error_4' => '
+        Error al insertar datos locales',
+        'error_5' => '
+        Error al insertar muestra de datos',
+    ],
+    'permission' => [
+        'process' => '
+        Configuración de carpetas de permisos',
+        'process_sucess' => '
+        Configuración de permiso de éxito!',
+        'error' => 'Error al inicializar la carpeta de permisos de configuración',
+    ],
+    'complete' => [
+        'process' => 'Prepárate para terminar',
+        'process_success' => '¡Terminada!',
+        'error' => '
+        Error al terminar',
+    ],
+
+    'validate' => [
+        'database_port_number' => '
+        El puerto de la base de datos es el número',
+        'database_port_required' => '
+        Puerto de base de datos requerido',
+        'database_host_required' => '
+        Base de datos del host requerida',
+        'database_name_required' => '
+        Se requiere el nombre de la base de datos',
+        'database_user_required' => '
+        Usuario de base de datos requerido',
+        'admin_url_required' => '
+        Ruta de administrador requerida',
+        'admin_user_required' => '
+        Se requiere usuario administrador',
+        'admin_password_required' => '
+        Se requiere contraseña de administrador',
+        'admin_email_required' => '
+        Se requiere correo electrónico de administrador',
+        'timezone_default_required' => '
+        Predeterminado de zona horaria requerido',
+        'language_default_required' => '
+        Idioma predeterminado requerido',
+    ],
+    'installing_button' => 'Instalación de S-Cart
+
+    ',
+    'database_host' => 'DB host',
+    'database_port' => 'DB port',
+    'database_name' => 'DB name',
+    'database_user' => 'DB user',
+    'database_password' => 'DB pasword',
+    'database_prefix' => 'DB prefix',
+    'database_prefix_help' => 'Ex: sc_, abc_,...',
+    'admin_url' => 'URL de administración',
+    'admin_user' => 'usuario administrador
+    ',
+    'admin_password' => 'clave de administrador',
+    'admin_email' => '
+    Correo electrónico del administrador',
+    'language_default' => 'Language',
+    'timezone_default' => 'Zona horaria
+    ',
+    'title' => 'Instalar S-Cart
+    ',
+    'installing' => 'Comenzar instalación
+    ',
+    'rename_error' => '
+    No se puede cambiar el nombre del archivo install.php. ¡Elimínelo o cámbiele el nombre!',
+    'terms' => '<span style="color:red">*</span>
+De acuerdo con los términos y condiciones',
+    'requirement_check' => '
+    Comprobación de requisitos',
+    'check_extension' => 'Comprobar extensión
+    ',
+    'check_writable' => '
+    cheque escribible',
+    'check_failed' => 'Fallido',
+    'check_ok' => 'OK',
+    'drop_db' => 'Drop table database if exist',
+    'exclude_sample' => '
+    Excluir muestra de datos',
+    'website_title' => 'Título de la página',
+    'website_title_place' => '
+    El nombre de tu sitio web',
+];
