d3c8661af314ba091f66ebc68d233183b3b2e1e5
johandry8919 <rojas.rojas440@gmail.com>
2023-02-16 09:52:18 -0400

    historial

--- resources/views/templates/waika-blue/account/historial_pagos.blade.php
+++ resources/views/templates/waika-blue/account/historial_pagos.blade.php
@@ -11,14 +11,13 @@ $layout_page = shop_profile
 
 @section('block_main_profile')
 <h6 class="title-store">{{ $title }}</h6>
-    
-
-      <table class="table box table-bordered" width="100%">
+      <table class="table table-hover box-body text-wrap table-bordered" width="100%">
         <thead>
           <tr>
             <th style="width: 50px;">No.</th>
             <th style="width: 100px;">Id Orden</th>
             <th>Pagado</th>
+            <th>Tasa de cambio</th>
             <th>Forma de pago</th>
             <th>estatus del pago</th>
             <th>{{ sc_language_render('common.created_at') }}</th>
@@ -27,8 +26,7 @@ $layout_page = shop_profile
         </thead>
         <tbody>
           @foreach($historial_pagos as $historial)
-            
-           
+
             <tr>
               @php
               $n = (isset($n)?$n:0);
@@ -37,18 +35,17 @@ $layout_page = shop_profile
             <td><span class="item_21_id">{{ $n }}</span></td>
             <td><span class="item_21_sku">{{ $historial->order_id }}</span></td>
             <td><span class="item_21_sku">{{ $historial->importe_pagado}}</span></td>
+            <td>{{$historial->tasa_cambio}}</td>
             <td><span class="item_21_sku">
               {!! isset($historial->metodo_pago->name ) ?$historial->metodo_pago->name  :'';  !!}
             </span></td>
+            
             <td><span class="item_21_sku">{!! isset($historial->estatus->name ) ?$historial->estatus->name  :'';  !!}</span></td>
             <td><span class="item_21_sku">{{ $historial->created_at->format('d/m/Y')}}</span></td>
             <td>              <a href="{{ sc_route('customer.order_detail', ['id' =>  $historial->order_id ]) }}"><i class="fa fa-indent" aria-hidden="true"></i> {{ sc_language_render('order.detail') }}</a><br>
             </td>
           </tr>
 
-          
-           
-          
 @endforeach
         </tbody>
       </table>
