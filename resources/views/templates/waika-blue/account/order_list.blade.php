25c3259663cc406d4b66ba4c965dc0c6758fd26d
johandry8919 <rojas.rojas440@gmail.com>
2023-03-01 15:08:20 -0400

    historial-pago-order_datail

--- resources/views/templates/waika-blue/account/order_list.blade.php
+++ resources/views/templates/waika-blue/account/order_list.blade.php
@@ -17,59 +17,62 @@
       <div class="panel-heading animate__animated animate__backInRight">
         <h5 class="panel-title">{{ $title }}</h5>
 
-         
-
-       
     </div>
 
-   
-      <table class="table table-responsive   table-fixed" width="100%">
-        <thead>
-          <tr>
-            <th style="width: 50px;">No.</th>
-            <th style="width: 100px;">ID</th>
-            <th>{{ sc_language_render('order.order_status') }}</th>
-            <th>{{ sc_language_render('common.created_at') }}</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($orders as $order)
-          @php
-          $n = (isset($n)?$n:0);
-          $n++;
+   <div class="table-responsive ">
+    <table class="table    table-fixed" width="100%">
+      <thead class="table-dark">
+        <tr>
+          <th style="width: 50px;">No.</th>
+          <th style="width: 100px;">Solicitud</th>
+          <th>{{ sc_language_render('order.order_status') }}</th>
+          <th>{{ sc_language_render('common.created_at') }}</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($orders as $order)
+        @php
+        $n = (isset($n)?$n:0);
+        $n++;
 
-          @endphp
        
-          <tr>
+        @endphp
+     
+        <tr class=" table-striped">
+
+        
+        
            
+          <td><span class="item_21_id">{{ $n }}</span></td>
+          <td><span class="item_21_sku">Nr°:{{$combenio['Nr_combenio'] }}</span></td>
+          <td>
+            
+            <span class="badge badge-{{ $mapStyleStatus[$order->status]??'' }}">{{$statusOrder[$order->status]}}
+            </span>
+          </td>
+          <td>
+            
+            {{ $order->created_at }}</td>
+          <td>
+            <a href="{{ sc_route('customer.order_detail', ['id' => $order->id ]) }}"><i class="fa fa-indent" aria-hidden="true"></i> {{ sc_language_render('order.detail') }}</a><br>
+           @if($order->modalidad_de_compra==0)
+            <a href="{{ sc_route('customer.reportar_pago', ['id' => $order->id ]) }}"><i class="fa fa-credit-card" aria-hidden="true"></i> Reportar pago</a>
+            @endif
 
-            <td><span class="item_21_id">{{ $n }}</span></td>
-            <td><span class="item_21_sku">#{{ $order->id }}</span></td>
-            <td>
-              
-              <span class="badge badge-{{ $mapStyleStatus[$order->status]??'' }}">{{$statusOrder[$order->status]}}
-              </span>
-            </td>
-            <td>
-              
-              {{ $order->created_at }}</td>
-            <td>
-              <a href="{{ sc_route('customer.order_detail', ['id' => $order->id ]) }}"><i class="fa fa-indent" aria-hidden="true"></i> {{ sc_language_render('order.detail') }}</a><br>
-             @if($order->modalidad_de_compra==0)
-              <a href="{{ sc_route('customer.reportar_pago', ['id' => $order->id ]) }}"><i class="fa fa-credit-card" aria-hidden="true"></i> Reportar pago</a>
-              @endif
+            <br>
+            @if ($order->status == 5 )
+            <a target="_blank" class="d-flex" href="{{route('borrador_cliente', ['id' => $order->id]) }}"><i style="display:{{$inconoAlert ?? "" }} ;"  class=""><img width="30px" class="img-fluid" src="/images/documento.gif" alt=""></i>Ver Convenio</a>
+            @endif
 
-              <br>
-              @if ($order->status == 5 )
-              <a target="_blank" class="d-flex" href="{{route('borrador_cliente', ['id' => $order->id]) }}"><i style="display:{{$inconoAlert ?? "" }} ;"  class=""><img width="30px" class="img-fluid" src="/images/documento.gif" alt=""></i>Ver Convenio</a>
-              @endif
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
 
-            </td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+   </div>
+      
       @endif
 
 
