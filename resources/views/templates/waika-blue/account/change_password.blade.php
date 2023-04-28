7c80804f4f4e28340a0e94873d61f3cf570a2552
Pedro Elías <pedrogm91@gmail.com>
2023-04-28 15:08:27 -0400

    my account

--- resources/views/templates/waika-blue/account/change_password.blade.php
+++ resources/views/templates/waika-blue/account/change_password.blade.php
@@ -11,53 +11,40 @@ $layout_page = shop_profile
 @section('block_main_profile')
     <h6 class="title-store">{{ $title }}</h6>
 
-            <form method="POST" action="{{ sc_route('customer.post_change_password') }}">
+            <form class="row gap-3" method="POST" action="{{ sc_route('customer.post_change_password') }}">
                 @csrf
 
-                <div class="form-group row {{ Session::has('password_old_error') ? ' has-error' : '' }}">
+                <div class="col-12 {{ Session::has('password_old_error') ? ' has-error' : '' }}">
                     <label for="password"
-                        class="col-md-4 col-form-label text-md-right">{{ sc_language_render('customer.password_old') }}</label>
-
-                    <div class="col-md-6">
+                        class="">{{ sc_language_render('customer.password_old') }}</label>
                         <input id="password" type="password" class="form-control" name="password_old" required>
-
                         @if(Session::has('password_old_error'))
                         <span class="help-block">{{ Session::get('password_old_error') }}</span>
                         @endif
 
-                    </div>
                 </div>
 
-                <div class="form-group row {{ $errors->has('password') ? ' has-error' : '' }}">
+                <div class="col-12 {{ $errors->has('password') ? ' has-error' : '' }}">
                     <label for="password"
-                        class="col-md-4 col-form-label text-md-right">{{ sc_language_render('customer.password_new') }}</label>
-
-                    <div class="col-md-6">
+                        class="">{{ sc_language_render('customer.password_new') }}</label>
                         <input id="password" type="password" class="form-control" name="password" required>
 
                         @if($errors->has('password'))
                         <span class="help-block">{{ $errors->first('password') }}</span>
                         @endif
-
-                    </div>
                 </div>
 
-                <div class="form-group row">
+                <div class="col-12">
                     <label for="password-confirm"
-                        class="col-md-4 col-form-label text-md-right">{{ sc_language_render('customer.password_confirm') }}</label>
-
-                    <div class="col-md-6">
+                        class="">{{ sc_language_render('customer.password_confirm') }}</label>
                         <input id="password-confirm" type="password" class="form-control"
                             name="password_confirmation" required>
-                    </div>
                 </div>
 
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="button button-lg button-secondary">
+                <div class="col-12 mb-0">
+                        <button type="submit" class="btn btn-primary">
                             {{ sc_language_render('customer.update_infomation') }}
                         </button>
-                    </div>
                 </div>
             </form>
 @endsection
--- resources/views/templates/waika-blue/account/lista_referencia.blade.php
+++ resources/views/templates/waika-blue/account/lista_referencia.blade.php
@@ -9,121 +9,93 @@ $layout_page = shop_profile
 @extends($sc_templatePath.'.account.layout')
 
 @section('block_main_profile')
-            <div class=" row ">
-                <div class="col-12 mb-4 text-center">
-                  <a class="btn btn-primary" data-toggle="modal" data-target="#myModal" href="#">Agregar Referencia Personal</a>  
-                </div>
-                <div class="col-12 col-md-3"></div>
-               <div class="col-md-9 col-12  text-center">
-                <table class="table table-hover   table-responsive " style="width: 100%;">
-                    <thead>
-                        <tr>
-                          <th>Nombre</th>
-                          <th>Apellido</th>
-                          <th>cedula</th>
-                          <th>Telefono</th>
-                          <th>Parentesco</th>
-                          <th>Acciones</th>
-                          
-                        </tr>
-                      </thead>
-               
-                      @foreach ($referencia as $ref)
-                  
-                    <tbody>
-                        <td>{{$ref->nombre_ref}}</td>
-                        <td>{{$ref->apellido_ref}}</td>
-                        <td>{{$ref->cedula_ref}}</td>
-                        <td>{{$ref->telefono}}</td>      
-                        <td>{{$ref->parentesco}}</td>      
-                        <td><span onclick="deleteItem('{!!$ref->id!!}');" title="Borrar" class="btn btn-flat btn-sm btn-danger"><i class="fas fa-trash-alt">X</i></span>
-                        </td>      
-                               
-                                
-                         
-                            
+  <div class="container">
+    <div class=" row ">
+      <div class="col-12 mb-4 text-end">
+        <a class="btn btn-primary" data-toggle="modal" data-target="#myModal" href="#">Agregar Referencia Personal</a>  
+      </div>
 
-                    </tbody>
-                    @endforeach
-    
-    
-               
-            </table>
-               </div>
-            
-              
+      <div class="col-12">
+        <div class="table-responsive">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>cedula</th>
+                <th>Telefono</th>
+                <th>Parentesco</th>
+                <th>Acciones</th>
+                
+              </tr>
+            </thead>
+                  
+            @foreach ($referencia as $ref)
+              <tbody>
+                  <td>{{$ref->nombre_ref}}</td>
+                  <td>{{$ref->apellido_ref}}</td>
+                  <td>{{$ref->cedula_ref}}</td>
+                  <td>{{$ref->telefono}}</td>      
+                  <td>{{$ref->parentesco}}</td>      
+                  <td><span onclick="deleteItem('{!!$ref->id!!}');" title="Borrar" class="btn btn-flat btn-sm btn-danger"><i class="fas fa-trash-alt"></i></span></td>      
+              </tbody>
+            @endforeach
+          </table>
         </div>
+      </div>      
+    </div>
+  </div>
      
-        <div class="container">
     
-          <div class="modal  mt-5" id="myModal" tabindex="-1" role="dialog">
-              <div class="modal-dialog  " role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                      
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                   
-                  </div>
-                  <h5 class="text-center">Referencia personal</h5>
-                  <div class="modal-body p-0">
+<div class="modal" id="myModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="text-center">Referencia personal</h5>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="container">
+          <form  method="POST" id="referencia">
+            <div class="row gap-3 justify-content-center">
+                <div class="col-12">
+                    <input value="{{ (old('nombre_ref' ?? ''))}}" id="nombre_ref" name="nombre_ref" type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="addon-wrapping">
+                </div>
+                <div class="col-12">
+                    <input value="{{ (old('apellido_ref' ?? ''))}}" id="apellido_ref" name="apellido_ref" type="text" class="form-control" placeholder="Apellido" aria-label="Apellido" aria-describedby="addon-wrapping">
+                </div>
       
-                     <div class="container">
-                      <form  method="POST" id="referencia">
-                          <div class="row justify-content-center  ">
-                              <div class="mt-4 col-12">
-                                  <div class="col-12 col mb-3">
-                                      <div class="input-group flex-nowrap">
-                                          <span class="input-group-text" id="addon-wrapping"></span>
-                                          <input value="{{ (old('nombre_ref' ?? ''))}}" id="nombre_ref" name="nombre_ref" type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="addon-wrapping">
-                                        </div></div>
-                                  <div class="col-12 mb-3">
-                                      <div class="input-group flex-nowrap">
-                                          <span class="input-group-text" id="addon-wrapping"></span>
-                                          <input value="{{ (old('apellido_ref' ?? ''))}}" id="apellido_ref" name="apellido_ref" type="text" class="form-control" placeholder="Apellido" aria-label="Apellido" aria-describedby="addon-wrapping">
-                                        </div></div>
-
-                                        <div class="col-12 mb-3">
-                                          <div class="input-group flex-nowrap">
-                                              <span class="input-group-text" id="addon-wrapping"></span>
-                                              <input value="{{ (old('cedula_ref' ?? ''))}}" id="cedula_ref" name="cedula_ref" type="text" class="form-control" placeholder="cedula" aria-label="cedula" aria-describedby="addon-wrapping">
-                                            </div></div>
-                                  <div class="col-12 mb-3">
-                                      <div class="input-group flex-nowrap">
-                                          <span class="input-group-text" id="addon-wrapping"></span>
-                                          <input value="{{ (old('telefono' ?? ''))}}" id="telefono_ref" name="telefono_ref" type="text" class="form-control" placeholder="Telefono" aria-label="Telefono" aria-describedby="addon-wrapping">
-                                        </div></div>
-                                  <div class="col-12 mb-3">
-                                      <div class="input-group flex-nowrap">
-                                          <span class="input-group-text" id="addon-wrapping"></span>
-                                          <input value="{{ (old('parentesco' ?? ''))}}" id="parentesco" name="parentesco" type="text" class="form-control" placeholder="Parentesco" aria-label="parentesco" aria-describedby="addon-wrapping">
-                                        </div></div>
-                              </div>
-                              <input type="hidden" name="">
-                             </div>
-                              
-      
-                             
-                             <div class="modal-footer mb-4">
-                              <button  type="submit" class="btn btn-default" data-dismiss="modal">Close</button>
-                              <button onclick="enViar_refencia()"  type="button" class="btn btn-primary">continuar</button>
-                            </div>
-                            <input type="hidden" id="id_usuario" name="id_usuario" value="{{$customer['id']}}">
-                          </form>
-                          </div>
-      
-                     </div>
-                    
-                    
-                  </div>
-                  
+                <div class="col-12">
+                    <input value="{{ (old('cedula_ref' ?? ''))}}" id="cedula_ref" name="cedula_ref" type="text" class="form-control" placeholder="cedula" aria-label="cedula" aria-describedby="addon-wrapping">
                 </div>
                 
-                
-                <!-- /.modal-content -->
-              </div>
+                <div class="col-12">
+                    <input value="{{ (old('telefono' ?? ''))}}" id="telefono_ref" name="telefono_ref" type="text" class="form-control" placeholder="Telefono" aria-label="Telefono" aria-describedby="addon-wrapping">
+                </div>
+              
+                <div class="col-12">
+                    <input value="{{ (old('parentesco' ?? ''))}}" id="parentesco" name="parentesco" type="text" class="form-control" placeholder="Parentesco" aria-label="parentesco" aria-describedby="addon-wrapping">
+                </div>
+              <input type="hidden" name="">
+            </div>
+            <div class="modal-footer">
+              <button  type="submit" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button onclick="enViar_refencia()"  type="button" class="btn btn-primary">Guardar</button>
+            </div>
+            <input type="hidden" id="id_usuario" name="id_usuario" value="{{$customer['id']}}">
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+<!-- /.modal-content -->
+</div>
               
        
-            </div>
 <script>
 
 function enViar_refencia(){
