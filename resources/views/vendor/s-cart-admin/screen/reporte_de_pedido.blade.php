7ce497f13ee57301ae1ff28b46aa55eabd86a647
johandry8919 <rojas.rojas440@gmail.com>
2024-03-12 16:25:46 -0400

    commit

--- resources/views/vendor/s-cart-admin/screen/reporte_de_pedido.blade.php
+++ resources/views/vendor/s-cart-admin/screen/reporte_de_pedido.blade.php
@@ -1,6 +1,5 @@
 @extends($templatePathAdmin.'layout')
 @section('main')
 
-
-<iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/8f8f0a3c-32e2-4d19-ae6c-42d309dabd05/page/qlD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+<iframe width="100%" height="800" src="https://lookerstudio.google.com/embed/reporting/8f8f0a3c-32e2-4d19-ae6c-42d309dabd05/page/qlD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
 @endsection
